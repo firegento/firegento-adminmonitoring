516517dea277861789c96ce55afa79d241a08b2e
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2013-09-07 18:01:09 +0200

    changed name of row from data to content added column content_diff added index

--- app/code/community/Firegento/AdminLogger/sql/firegento_adminlogger/upgrade-1.0.0-1.0.1.php
+++ app/code/community/Firegento/AdminLogger/sql/firegento_adminlogger/upgrade-1.0.0-1.0.1.php
@@ -12,9 +12,29 @@ $installer->getConnection()->addIndex(
         ),
         Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX
     ),
-    $installer->getTable('firegento_adminlogger/history'),
     array(
          'object_type', 'object_id'
     ),
     Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX
+);
+
+$installer->getConnection()->changeColumn(
+    $installer->getTable('firegento_adminlogger/history'),
+    'data',
+    'content',
+    array(
+         'type'    => Varien_Db_Ddl_Table::TYPE_TEXT,
+         'size'    => null,
+         'comment' => 'data of changed entity'
+    )
+);
+
+$installer->getConnection()->addColumn(
+    $installer->getTable('firegento_adminlogger/history'),
+    'content_diff',
+    array(
+         'type'    => Varien_Db_Ddl_Table::TYPE_TEXT,
+         'size'    => null,
+         'comment' => 'changed data of entity'
+    )
 );
