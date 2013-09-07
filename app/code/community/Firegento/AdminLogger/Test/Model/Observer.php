1e1c794c99a6262b9eea67ea5c4d9a0530718376
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2013-09-07 18:05:45 +0200

    replaced admin/session with mock without calling constructor

--- app/code/community/Firegento/AdminLogger/Test/Model/Observer.php
+++ app/code/community/Firegento/AdminLogger/Test/Model/Observer.php
@@ -10,6 +10,21 @@ class Firegento_AdminLogger_Test_Model_Observer extends EcomDev_PHPUnit_Test_Cas
     {
         parent::setUp();
         $this->_object = Mage::getModel('firegento_adminlogger/observer');
+
+        // set admin/session
+        $this->_setAdminSession();
+    }
+
+    /**
+     * set admin session with session
+     */
+    protected function _setAdminSession()
+    {
+        $sessionMock = $this->getModelMockBuilder('admin/session')
+            ->disableOriginalConstructor() // This one removes session_start and other methods usage
+            ->setMethods(null) // Enables original methods usage, because by default it overrides all methods
+            ->getMock();
+        $this->replaceByMock('singleton', 'admin/session', $sessionMock);
     }
 
     /**
@@ -36,6 +51,24 @@ class Firegento_AdminLogger_Test_Model_Observer extends EcomDev_PHPUnit_Test_Cas
 
         $mock = $this->getModelMock('firegento_adminlogger/history');
         $mock->expects($this->never())->method('save');
+        $this->replaceByMock('model', 'firegento_adminlogger/history', $mock);
+
+        Mage::getModel('firegento_adminlogger/observer')->modelSaveAfter($observer);
+    }
+
+    public function testHistorySavesWithCustomer()
+    {
+        $customer = new Mage_Customer_Model_Customer();
+        $customer->setEmail('test@example.com');
+        $customer->setFirstname('Johnny');
+        $customer->setLastname('Doels');
+        $customer->setPassword('1234567890');
+
+        $data = array('object' => $customer);
+        $observer = $this->_createObserver($data);
+
+        $mock = $this->getModelMock('firegento_adminlogger/history');
+        $mock->expects($this->once())->method('save');
         $this->replaceByMock('model', 'firegento_adminlogger/history', $mock);
 
         Mage::getModel('firegento_adminlogger/observer')->modelSaveAfter($observer);
