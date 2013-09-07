493725107d6179688b73ebfe004e786ddd5b517d
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2013-09-07 20:11:58 +0200

    added tests for saving

--- app/code/community/Firegento/AdminLogger/Test/Model/Observer.php
+++ app/code/community/Firegento/AdminLogger/Test/Model/Observer.php
@@ -56,21 +56,39 @@ class Firegento_AdminLogger_Test_Model_Observer extends EcomDev_PHPUnit_Test_Cas
         Mage::getModel('firegento_adminlogger/observer')->modelSaveAfter($observer);
     }
 
-    public function testHistorySavesWithCustomer()
+    /**
+     * @dataProvider dataProvider
+     * @loadExpectation
+     */
+    public function testHistorySavesWithCustomer($mail, $firstname, $lastname, $password)
     {
         $customer = new Mage_Customer_Model_Customer();
-        $customer->setEmail('test@example.com');
-        $customer->setFirstname('Johnny');
-        $customer->setLastname('Doels');
-        $customer->setPassword('1234567890');
+        $customer->setEmail($mail);
+        $customer->setFirstname($firstname);
+        $customer->setLastname($lastname);
+        $customer->setPasswordHash('1234567890ß');
 
         $data = array('object' => $customer);
         $observer = $this->_createObserver($data);
 
-        $mock = $this->getModelMock('firegento_adminlogger/history');
+        $mock = $this->getModelMock('firegento_adminlogger/history', null);
         $mock->expects($this->once())->method('save');
         $this->replaceByMock('model', 'firegento_adminlogger/history', $mock);
 
         Mage::getModel('firegento_adminlogger/observer')->modelSaveAfter($observer);
+
+        $data = $mock->getData();
+        $this->assertRegExp('#^[0-9]*$#', $mock->getHistoryId());
+        $this->assertRegExp(
+            '/^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/',
+            $mock->getCreatedAt()
+        );
+        unset($data['created_at'], $data['history_id']); // remove changing attributes
+        $this->assertEquals(
+            $this->expected("$mail-$firstname-$lastname-$password")->getData(),
+            $data
+        );
+
+
     }
 }
