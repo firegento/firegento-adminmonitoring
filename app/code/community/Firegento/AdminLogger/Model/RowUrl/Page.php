fb9bdd072c8d61ed8eb567200305b44c4517ef2e
airbone42 <tobias.zander@sitewards.com>
2013-09-08 13:25:56 +0200

    don't show row url for deleted models

--- app/code/community/Firegento/AdminLogger/Model/RowUrl/Page.php
+++ app/code/community/Firegento/AdminLogger/Model/RowUrl/Page.php
@@ -6,16 +6,22 @@ class Firegento_AdminLogger_Model_RowUrl_Page {
      * @param Varien_Event_Observer $observer
      */
     public function setRowUrl(Varien_Event_Observer $observer) {
-        $model = $observer->getHistory()->getOriginalModel();
-        if ($model instanceof Mage_Cms_Model_Page) {
-            $observer->getTransport()->setRowUrl(
-                Mage::getModel('adminhtml/url')->getUrl(
-                    'adminhtml/cms_page/edit',
-                    array(
-                        'page_id' => $model->getId(),
+        /**
+         * @var $history Firegento_AdminLogger_Model_History
+         */
+        $history = $observer->getHistory();
+        if (!$history->isDelete()) {
+            $model = $history->getOriginalModel();
+            if ($model instanceof Mage_Cms_Model_Page) {
+                $observer->getTransport()->setRowUrl(
+                    Mage::getModel('adminhtml/url')->getUrl(
+                        'adminhtml/cms_page/edit',
+                        array(
+                            'page_id' => $model->getId(),
+                        )
                     )
-                )
-            );
+                );
+            }
         }
     }
 }
--- app/code/community/Firegento/AdminLogger/Model/RowUrl/Product.php
+++ app/code/community/Firegento/AdminLogger/Model/RowUrl/Product.php
@@ -6,17 +6,23 @@ class Firegento_AdminLogger_Model_RowUrl_Product {
      * @param Varien_Event_Observer $observer
      */
     public function setRowUrl(Varien_Event_Observer $observer) {
-        $model = $observer->getHistory()->getOriginalModel();
-        if ($model instanceof Mage_Catalog_Model_Product) {
-            $observer->getTransport()->setRowUrl(
-                Mage::getModel('adminhtml/url')->getUrl(
-                    'adminhtml/catalog_product/edit',
-                    array(
-                        'id'    => $model->getId(),
-                        'store' => $model->getStoreId(),
+        /**
+         * @var $history Firegento_AdminLogger_Model_History
+         */
+        $history = $observer->getHistory();
+        if (!$history->isDelete()) {
+            $model = $history->getOriginalModel();
+            if ($model instanceof Mage_Catalog_Model_Product) {
+                $observer->getTransport()->setRowUrl(
+                    Mage::getModel('adminhtml/url')->getUrl(
+                        'adminhtml/catalog_product/edit',
+                        array(
+                            'id'    => $model->getId(),
+                            'store' => $model->getStoreId(),
+                        )
                     )
-                )
-            );
+                );
+            }
         }
     }
 }
