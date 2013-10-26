51097bd1a39ae586a096713e27724bf78dbec32f
Rouven Alexander Rieker <therouv@googlemail.com>
2013-10-26 22:30:53 +0200

    ! Travis & fixed unit tests

--- src/app/code/community/FireGento/AdminMonitoring/Test/Model/Observer.php
+++ src/app/code/community/FireGento/AdminMonitoring/Test/Model/Observer.php
@@ -28,7 +28,7 @@
 class FireGento_AdminMonitoring_Test_Model_Observer extends EcomDev_PHPUnit_Test_Case
 {
     /**
-     * @var FireGento_AdminMonitoring_Model_Observer
+     * @var FireGento_AdminMonitoring_Model_Observer_Model_Save
      */
     protected $_object;
 
@@ -38,7 +38,7 @@ class FireGento_AdminMonitoring_Test_Model_Observer extends EcomDev_PHPUnit_Test
     protected function setUp()
     {
         parent::setUp();
-        $this->_object = Mage::getModel('firegento_adminmonitoring/observer');
+        $this->_object = Mage::getModel('firegento_adminmonitoring/observer_model_save');
 
         // set admin/session
         $this->_setAdminSession();
@@ -83,7 +83,7 @@ class FireGento_AdminMonitoring_Test_Model_Observer extends EcomDev_PHPUnit_Test
         $mock->expects($this->never())->method('save');
         $this->replaceByMock('model', 'firegento_adminmonitoring/history', $mock);
 
-        Mage::getModel('firegento_adminmonitoring/observer')->modelSaveAfter($observer);
+        $this->_object->modelAfter($observer);
     }
 
     /**
@@ -94,11 +94,12 @@ class FireGento_AdminMonitoring_Test_Model_Observer extends EcomDev_PHPUnit_Test
     public function testHistorySavesWithCustomer(
         $id, $mail = null, $firstname = null, $lastname = null, $password = null
     ) {
+        /* @var $customer Mage_Customer_Model_Customer */
         $customer = Mage::getModel('customer/customer');
+
         if ($id !== null) {
             $customer->load($id);
         }
-
         if (!empty($mail)) {
             $customer->setEmail($mail);
         }
@@ -112,22 +113,25 @@ class FireGento_AdminMonitoring_Test_Model_Observer extends EcomDev_PHPUnit_Test
             $customer->setPasswordHash($password);
         }
 
-
         $data = array('object' => $customer);
         $observer = $this->_createObserver($data);
 
         $mock = $this->getModelMock('firegento_adminmonitoring/history', null);
         $mock->expects($this->once())->method('save');
         $this->replaceByMock('model', 'firegento_adminmonitoring/history', $mock);
-        $this->_object->modelSaveBefore($observer);
-        $this->_object->modelSaveAfter($observer);
+
+        $this->_object->modelBefore($observer);
+        $this->_object->modelAfter($observer);
         $data = $mock->getData();
+
         $this->assertRegExp('#^[0-9]*$#', $mock->getHistoryId());
         $this->assertRegExp(
             '/^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/',
             $mock->getCreatedAt()
         );
+
         unset($data['created_at'], $data['history_id']); // remove changing attributes
+
         $this->assertEquals(
             $this->expected("$id-$mail-$firstname-$lastname-$password")->getData(),
             $data
